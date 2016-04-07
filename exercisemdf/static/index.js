a8f35888581b1d2d1676a5b21f255ec698e18b45
lfwen <lfwen@ubuntu.(none)>
2016-04-07 10:18:51 +0800

    Revert "增加全屏功能"
    This reverts commit 97e9a5b488f7d8d2178a02ddccbf0e5ea43ce190.

--- exercisemdf/static/index.js
+++ exercisemdf/static/index.js
@@ -6,17 +6,4 @@ function ExerciseMdfXBlock(runtime, element) {
         ajaxData.type = 'POST';
         $.ajax(ajaxData);
     };
-
-    $(element).on('click', '.fullscreen', function(event) {
-        var frame = $('#exercisemdf')[0];
-        if (frame.requestFullscreen) {
-            frame.requestFullscreen();
-        }
-        else if (frame.mozRequestFullScreen) {
-            frame.mozRequestFullScreen();
-        }
-        else if (frame.webkitRequestFullscreen) {
-            frame.webkitRequestFullscreen();
-        }
-    });
 }
